ea49c260bec490d644bc88fdbe05c25fd283d1d0
Alice <aliceykim0828@gmail.com>
2018-02-20 23:37:39 -0800

    Update the speed and timing of fade-in on scroll

--- animation-on-scroll.js
+++ animation-on-scroll.js
@@ -4,9 +4,11 @@ $(document).ready(function () {
 		/* Checks the location of each desired element */
 		$('.column-container').each(function (i) {
 			var top_of_object = $(this).position().top;
+			console.log("top_of_object: " + top_of_object)
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			console.log(bottom_of_window)
 			/* If the object is completely visible in the window, fade it it */
-			if (bottom_of_window > top_of_object - 10) {
+			if (bottom_of_window > top_of_object - 50) {
 				$(this).animate({
 					'opacity': '1'
 				}, 2000);
