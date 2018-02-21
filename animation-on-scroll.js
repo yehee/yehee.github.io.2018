1ab0f38875a83c68ff5225241c95e05758800646
Alice <aliceykim0828@gmail.com>
2018-02-20 23:23:14 -0800

    Update animation-on-scroll.js

--- animation-on-scroll.js
+++ animation-on-scroll.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		/* Checks the location of each desired element */
 		$('.column-container').each(function (i) {
-			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var top_of_object = $(this).position().top;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			/* If the object is completely visible in the window, fade it it */
-			if (bottom_of_window > bottom_of_object) {
+			if (bottom_of_window > top_of_object + 50) {
 				$(this).animate({
 					'opacity': '1'
 				}, 2000);
