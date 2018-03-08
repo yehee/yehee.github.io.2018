356be216592bb21b22c32139d1548d614e147871
Alice Kim <aliceykim0828@gmail.com>
2018-03-07 21:55:46 -0800

    Add percent bar

--- animation-on-scroll.js
+++ animation-on-scroll.js
@@ -14,5 +14,23 @@ $(document).ready(function () {
 				}, 2000);
 			}
 		});
+		$('.percentage-bar').each(function (i) {
+			var top_of_object = $(this).position().top;
+			console.log("top_of_object: " + top_of_object)
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			console.log(bottom_of_window)
+			/* If the object is completely visible in the window, fade it it */
+			if (bottom_of_window > top_of_object - 50) {
+				var target = '80%';
+				if ($(this).attr('id') == 'test') {
+					target = '70%';
+				} else if ($(this).attr('id') == 'javascript') {
+					target = '60%';
+				}
+				$(this).animate({
+					'width': target
+				}, 2000);
+			}
+		});
 	});
 });
