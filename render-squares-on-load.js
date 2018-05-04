6ef680a3f057c973d2962b2006c823fcf790cfd0
Alice Kim <alice828@mail.ubc.ca>
2018-05-03 21:35:59 -0700

    Remove checking for view block

--- render-squares-on-load.js
+++ render-squares-on-load.js
@@ -38,63 +38,60 @@ function updateNumColumns() {
 }
 
 function renderProfile() {
-    var profilesBlock = document.getElementById("block-views-profiles-list-block-1");
-    if (profilesBlock != null) {
-        var profilesList = document.getElementsByClassName("grid-item");
-        var positionX = 0;
-        var currentY = 0;
-        var positionY = [0, 0, 0, 0];
-        for (var i = 0; i < profilesList.length; i++) {
-            var currentProfile = profilesList[i];
-            while (positionY[positionX] > currentY) {
-                positionX = (positionX + 1) % numColumns;
-                if (positionX == 0) {
-                    currentY++;
-                }
-            }
-            if (positionX == (numColumns - 1) || positionY[positionX + 1] > currentY) {
-                var range = 2;
-            } else {
-                var range = 3;
-            }
-            var chosen = Math.floor(Math.random() * range);
-            var images = profilesList[i].getElementsByTagName("img");
-            for (var j = 0; j < images.length; j++) {
-                if (j == chosen) {
-                    images[j].style.display = "block";
-                } else {
-                    images[j].style.display = "none";
-                }
-            }
-            profilesList[i].style.width = width[chosen] + "px";
-            profilesList[i].style.height = height[chosen] + "px";
-            profilesList[i].style.position = "absolute";
-
-            profilesList[i].style.left = positionX * 278 + "px";
-            profilesList[i].style.top = positionY[positionX] * 270 + "px";
-            images[chosen].style.width = width[chosen] + "px";
-            images[chosen].style.height = height[chosen] + "px";
-            if (chosen == 0) {
-                positionY[positionX] += 1;
-                positionX = (positionX + 1) % numColumns;
-            } else if (chosen == 1) {
-                positionY[positionX] += 2;
-                positionX = (positionX + 1) % numColumns;
-            } else {
-                // chosen == 2
-                positionY[positionX] += 1;
-                positionY[positionX + 1] += 1;
-                positionX = (positionX + 2) % numColumns;
-            }
+    var profilesList = document.getElementsByClassName("grid-item");
+    var positionX = 0;
+    var currentY = 0;
+    var positionY = [0, 0, 0, 0];
+    for (var i = 0; i < profilesList.length; i++) {
+        var currentProfile = profilesList[i];
+        while (positionY[positionX] > currentY) {
+            positionX = (positionX + 1) % numColumns;
             if (positionX == 0) {
                 currentY++;
             }
-            //assign random background color
-            var backgroundColor = backgroundColors[Math.floor(backgroundColors.length * Math.random())];
-            images[chosen].style.background = backgroundColor;
         }
-        var viewContent = document.getElementsByClassName("view-content");
-        viewContent[0].style.position = "relative";
-        viewContent[0].style.height = Math.max(...positionY) * 270 + "px";
+        if (positionX == (numColumns - 1) || positionY[positionX + 1] > currentY) {
+            var range = 2;
+        } else {
+            var range = 3;
+        }
+        var chosen = Math.floor(Math.random() * range);
+        var images = profilesList[i].getElementsByTagName("img");
+        for (var j = 0; j < images.length; j++) {
+            if (j == chosen) {
+                images[j].style.display = "block";
+            } else {
+                images[j].style.display = "none";
+            }
+        }
+        profilesList[i].style.width = width[chosen] + "px";
+        profilesList[i].style.height = height[chosen] + "px";
+        profilesList[i].style.position = "absolute";
+
+        profilesList[i].style.left = positionX * 278 + "px";
+        profilesList[i].style.top = positionY[positionX] * 270 + "px";
+        images[chosen].style.width = width[chosen] + "px";
+        images[chosen].style.height = height[chosen] + "px";
+        if (chosen == 0) {
+            positionY[positionX] += 1;
+            positionX = (positionX + 1) % numColumns;
+        } else if (chosen == 1) {
+            positionY[positionX] += 2;
+            positionX = (positionX + 1) % numColumns;
+        } else {
+            // chosen == 2
+            positionY[positionX] += 1;
+            positionY[positionX + 1] += 1;
+            positionX = (positionX + 2) % numColumns;
+        }
+        if (positionX == 0) {
+            currentY++;
+        }
+        //assign random background color
+        var backgroundColor = backgroundColors[Math.floor(backgroundColors.length * Math.random())];
+        images[chosen].style.background = backgroundColor;
     }
+    var viewContent = document.getElementsByClassName("view-content");
+    viewContent[0].style.position = "relative";
+    viewContent[0].style.height = Math.max(...positionY) * 270 + "px";
 }
