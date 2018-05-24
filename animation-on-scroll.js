9153c7fe78c4ffa240ba4e56470cdf58ce7980fa
alice-0-kim <aliceykim0828@gmail.com>
2018-05-24 16:59:56 -0700

    add video

--- animation-on-scroll.js
+++ animation-on-scroll.js
@@ -4,9 +4,7 @@ $(document).ready(function () {
 		/* Checks the location of each desired element */
 		$('.column-container').each(function (i) {
 			var top_of_object = $(this).position().top;
-			console.log("top_of_object: " + top_of_object)
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
-			console.log(bottom_of_window)
 			/* If the object is completely visible in the window, fade it it */
 			if (bottom_of_window > top_of_object - 50) {
 				$(this).animate({
@@ -16,9 +14,7 @@ $(document).ready(function () {
 		});
 		$('.percentage-bar').each(function (i) {
 			var top_of_object = $(this).position().top;
-			console.log("top_of_object: " + top_of_object)
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
-			console.log(bottom_of_window)
 			/* If the object is completely visible in the window, fade it it */
 			if (bottom_of_window > top_of_object - 50) {
 				var target = '80%';
