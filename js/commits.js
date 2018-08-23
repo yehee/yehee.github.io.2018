b25ff916a2f43a639e5e04a833762e9b389b1ad5
alice-0-kim <aliceykim0828@gmail.com>
2018-08-23 16:59:24 -0700

    Update

--- js/commits.js
+++ js/commits.js
@@ -44,8 +44,6 @@ var commits = new Vue({
       xhr.open('GET', apiURL1 + self.directory + apiURL2 + self.currentBranch)
       xhr.onload = function () {
         self.commits = JSON.parse(xhr.responseText)
-        // console.log(self.commits)
-        // console.log(self.commits[0].html_url)
       }
       xhr.send()
     }
--- js/repos.js
+++ js/repos.js
@@ -3,19 +3,49 @@ var apiURL = 'https://api.github.com/users/alice-0-kim/repos'
 var repos = new Vue ({
   el: '#repos',
   data: {
-    projects: []
+    projects: [],
+  	limit: 9,
+  	curr: 1,
+  	total: 0,
   },
   created: function () {
     var self = this
     axios.get(apiURL)
     .then(function (response) {
-      self.projects = response.data
-      console.log(response.data)
+      for (var project of response.data) {
+      	if (!project.fork) {
+      		self.projects.push(project)
+      	}
+      }
+      self.total = Math.ceil(self.projects.length / self.limit)
       return response.data
     })
     .catch(function (error) {
       console.log(error)
       return
     })
+  },
+  computed: {
+  	pizza: function () {
+  		console.log((this.curr - 1) * this.limit)
+  		console.log(this.curr * this.limit)
+  		return this.projects.slice((this.curr - 1) * this.limit, this.curr * this.limit)
+  	}
+  },
+  methods: {
+  	prev: function () {
+  		if (this.curr == 1) {
+  			alert('This is the first page!')
+  		} else {
+  			this.curr --
+  		}
+  	},
+  	next: function () {
+			if (this.curr == this.total) {
+				alert('This is the last page!')
+			} else {
+				this.curr ++
+			}
+  	}
   }
 });
